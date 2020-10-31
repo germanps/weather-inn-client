90c94ab9049277363ca7bfb6182644de69a1f28e
German Pla <germanps@outlook.es>
2020-10-31 09:09:30 +0100

    Save token data in apiContext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { ApolloProvider } from "@apollo/client"
 import client from "./config/apollo"
 import Auth from './pages/Auth'
+import AuthContext from './context/AuthContext'
 import { getToken } from './utils/token'
 import './App.scss';
 
 
 export default function App() {
+  const [auth, setAuth] = useState(undefined)
+
   useEffect(() => {
     //if userToken is set redirect to home page
     const token = getToken()
     if (!token) setAuth(null)
     else setAuth(token)
   }, [])
-  const [auth, setAuth] = useState(undefined)
+
+  const logout = () => {
+    console.log('cerrar sesión')
+  }
+
+  const setUser = user => setAuth(user)
+  //avoid new render with useMemo
+  const authData = useMemo(
+    () => ({
+      auth,
+      logout,
+      setUser
+    }), [auth]
+  )
 
   return (
     <ApolloProvider client={client}>
-      {!auth ? <Auth /> : <h1>Estas logeado</h1>}
+      <AuthContext.Provider value={authData}>
+        {!auth ? <Auth /> : <h1>Estas logeado</h1>}
+      </AuthContext.Provider>
     </ApolloProvider>
   );
 }
--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -11,12 +11,17 @@ import * as Yup from 'yup'
 import { useMutation } from '@apollo/client'
 import { LOGIN } from '../../../gql/user'
 import { setToken } from '../../../utils/token'
+import { useAuth } from '../../../hooks/useAuth'
 import "./LoginForm.scss"
 
 export default function LoginForm() {
     const [error, setError] = useState('')
     const [login] = useMutation(LOGIN)
 
+    //get function from context
+    const { setUser } = useAuth()
+
+    //handler data with formik
     const formik = useFormik({
         initialValues: initialFormValues(),
         validationSchema: Yup.object({
@@ -34,6 +39,7 @@ export default function LoginForm() {
                 })
                 const { token } = data.login
                 setToken(token)
+                setUser(token)//redirect to home
             } catch (error) {
                 setError(error.message)
             }
